b490299fa27caef9a3da07bf2801c739a3f9d0e8
Sarah Davies <sarahdavies186@gmail.com>
2025-01-13 21:58:19 +0000

    modules 1 to 4

--- Program.cs
+++ Program.cs
@@ -1,4 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.Write("Congratulations");
-Console.Write(" Sarah");
+Console.WriteLine("This is the first line");
+Console.Write("This is");
+Console.Write(" the second line");
+Console.WriteLine('b');
+Console.WriteLine(123);
+Console.WriteLine(0.25F);
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+
+// string firstName = "Bob";
+// firstName = "Bob";
+// firstName = "Liem";
+// Console.WriteLine(firstName);
+
+var message = "Hello world!";
+
+Console.Write(message);
+
+string firstName = "Bob";
+int numOfMessages = 3;
+decimal temp = 34.4m;
+
+Console.Write("Hello, ");
+Console.Write(firstName);
+Console.Write("! You have");
+Console.Write(numOfMessages);
+Console.Write(" in your inbox. The temoerature is ");
+Console.Write(temp);
+Console.Write(" celsisus.");
+
+Console.WriteLine("Generating invoices for customer \"Contoso Corp\" ...\n");
+Console.WriteLine("Invoice: 1021\t\tComplete!");
+Console.WriteLine("Invoice: 1022\t\tComplete!");
+Console.WriteLine("\nOutput Directory:\t");
+Console.Write(@"c:\invoices");
+
+// To generate Japanese invoices:
+// Nihon no seikyū-sho o seisei suru ni wa:
+Console.Write("\n\n\u65e5\u672c\u306e\u8acb\u6c42\u66f8\u3092\u751f\u6210\u3059\u308b\u306b\u306f\uff1a\n\t");
+// User command to run an application
+Console.WriteLine(@"c:\invoices\app.exe -j");
+
+// string firstName = "Bob";
+string greeting = "Hello";
+string message2 = $"{greeting} {firstName}!";
+Console.WriteLine(message2);
+
+
+string projectName = "ACME";
+string englishLocation = $@"c:\Exercise\{projectName}\data.txt";
+Console.WriteLine($"View English output:\n\t\t{englishLocation}\n");
+
+string russianMessage = "\u041f\u043e\u0441\u043c\u043e\u0442\u0440\u0435\u0442\u044c \u0440\u0443\u0441\u0441\u043a\u0438\u0439 \u0432\u044b\u0432\u043e\u0434";
+string russianLocation = $@"c:\Exercise\{projectName}\ru-RU\data.txt";
+Console.WriteLine($"{russianMessage}:\n\t\t{russianLocation}\n");
+
